34992d88d93dffa959088738490afb6e7e8dd799
Your Name <pistoni@example.com>
2025-12-16 09:15:40 +0100

    cartonize 32x32

--- run_train.py
+++ run_train.py
@@ -70,6 +70,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     os.makedirs(trainOutput_dir, exist_ok=True)
+    os.makedirs(models_dir, exist_ok=True)
 
     dataset = PairedGrayDataset(args.dataroot, 'Train_Samples', block_size)
     dataloader = DataLoader(dataset, batch_size, shuffle=True, num_workers=4, pin_memory=True)
